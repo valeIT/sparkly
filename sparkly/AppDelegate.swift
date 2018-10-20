7ea4581663707aa57618f3d1e51612c39d50f96e
Valentino Urbano <valentino@valentinourbano.com>
2018-10-20 10:38:52 +0200

    init

---
+++ sparkly/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sparkly
+//
+//  Created by Valentino Urbano on 20/10/2018.
+//  Copyright © 2018 Valentino Urbano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ sparkly/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sparkly
+//
+//  Created by Valentino Urbano on 20/10/2018.
+//  Copyright © 2018 Valentino Urbano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
